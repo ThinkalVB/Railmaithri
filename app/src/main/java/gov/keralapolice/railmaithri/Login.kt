50622796de7858c30956b0a5a5fa71638d27f7e9
Thinkal V B <thinkalvb@gmail.com>
2023-06-26 16:04:47 +0530

    Initial commit

---
+++ app/src/main/java/gov/keralapolice/railmaithri/Login.kt
@@ -0,0 +1,36 @@
+package gov.keralapolice.railmaithri
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        supportActionBar!!.hide()
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        val appPermissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.CALL_PHONE
+        )
+        val neededPermissions = ArrayList<String>()
+        for (permission in appPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                neededPermissions.add(permission)
+            }
+        }
+        if(neededPermissions.isNotEmpty()) {
+            val requestCode = 108
+            ActivityCompat.requestPermissions(this, neededPermissions.toTypedArray(), requestCode)
+        }
+    }
+}
