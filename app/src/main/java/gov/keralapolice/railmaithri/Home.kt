ca7cf2f35e22f970dc1cf5f18d35b8aa1eac2e5e
Thinkal V B <thinkalvb@gmail.com>
2023-07-22 15:42:00 +0530

    Location tracking implemented

--- app/src/main/java/gov/keralapolice/railmaithri/Home.kt
+++ app/src/main/java/gov/keralapolice/railmaithri/Home.kt
@@ -1,7 +1,13 @@
 package gov.keralapolice.railmaithri
 
+import android.Manifest
+import android.app.ActivityManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,28 +16,36 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import gov.keralapolice.railmaithri.services.TrackingService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import org.json.JSONObject
+import java.util.Calendar
 
 class Home : AppCompatActivity() {
-    private lateinit var token:         String
-    private lateinit var profile:       JSONObject
-    private lateinit var clientNT:      OkHttpClient
-    private lateinit var logoutBT:      ImageView
-    private lateinit var savedDataBT:   ImageView
+    private lateinit var token: String
+    private var officerID: Int = 0
+    private lateinit var profile: JSONObject
+    private lateinit var clientNT: OkHttpClient
+    private lateinit var logoutBT: ImageView
+    private lateinit var savedDataBT: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
         supportActionBar!!.hide()
 
-        clientNT  = OkHttpClient().newBuilder().build()
-        profile   = JSONObject(Helper.getData(this, Storage.PROFILE)!!)
-        token     = Helper.getData(this, Storage.TOKEN)!!
-        logoutBT  = findViewById(R.id.logout)
+        clientNT = OkHttpClient().newBuilder().build()
+        profile = JSONObject(Helper.getData(this, Storage.PROFILE)!!)
+        officerID = profile.getInt("id")
+        token = Helper.getData(this, Storage.TOKEN)!!
+        logoutBT = findViewById(R.id.logout)
         savedDataBT = findViewById(R.id.saved_data)
+
 
         findViewById<TextView>(R.id.officer_name).text = profile.getString("username")
         savedDataBT.setOnClickListener {
@@ -39,7 +53,7 @@ class Home : AppCompatActivity() {
             startActivity(intent)
         }
         logoutBT.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {  logout() }
+            CoroutineScope(Dispatchers.IO).launch { logout() }
         }
 
         // Passenger statistics
@@ -65,21 +79,93 @@ class Home : AppCompatActivity() {
             intent.putExtra("mode", Mode.SEARCH_FORM)
             startActivity(intent)
         }
+
+        startTracking()
     }
+
+    private fun startTracking() {
+        handleLocationUpdates()
+    }
+
+    private fun handleLocationUpdates() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+            } else {
+                startLocationService()
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+            } else {
+                startLocationService()
+            }
+        }
+    }
+
+    private fun startLocationService() {
+        if (!isLocationServiceRunning()) {
+            val intent = Intent(applicationContext, TrackingService::class.java)
+            intent.action = "startLocationService"
+            startService(intent)
+            Toast.makeText(this, "Location service started", Toast.LENGTH_SHORT).show()
+            val c = Calendar.getInstance()
+            c[Calendar.HOUR_OF_DAY] = c[Calendar.HOUR_OF_DAY] + 1
+            c[Calendar.MINUTE] = c[Calendar.MINUTE]
+            c[Calendar.SECOND] = 0
+        }
+    }
+
+    private fun isLocationServiceRunning(): Boolean {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        if (activityManager != null) {
+            for (service in activityManager.getRunningServices(Int.MAX_VALUE)) {
+                if (LocationServices::class.java.name == service.service.className) {
+                    if (service.foreground) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+        return false
+    }
+
+    private fun stopLocationService() {
+        if (!isLocationServiceRunning()) {
+            val intent = Intent(applicationContext, TrackingService::class.java)
+            intent.action = "stopLocationService"
+            startService(intent)
+            Toast.makeText(this, "Location service stopped", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun logout() {
         Handler(Looper.getMainLooper()).post {
-            logoutBT.isClickable   = false
+            logoutBT.isClickable = false
         }
 
         val data = JSONObject()
         data.put("app_version", App.APP_VERSION)
 
         try {
-            val request  = API.post(URL.MOBILE_LOGOUT, data, token)
+            val request = API.post(URL.MOBILE_LOGOUT, data, token)
             val response = clientNT.newCall(request).execute()
             Log.e("Railmaithri", response.code.toString())
             if (response.isSuccessful || response.code == 401) {
                 Helper.saveData(this, Storage.TOKEN, "")
+                stopLocationService()
+                unregisterReceiver(locationStateReceiver)
                 startActivity(Intent(this, Login::class.java))
                 finish()
             } else {
@@ -91,8 +177,63 @@ class Home : AppCompatActivity() {
             Helper.showToast(this, "Server unreachable !!")
         } finally {
             Handler(Looper.getMainLooper()).post {
-                logoutBT.isClickable   = true
+                logoutBT.isClickable = true
             }
         }
+    }
+
+    private fun saveLocationDataToServer(
+        latitude: String,
+        longitude: String,
+        accuracy: String,
+        speed: String,
+        heading: String,
+        altitude: String
+    ) {
+        try {
+            Toast.makeText(this@Home, "API", Toast.LENGTH_SHORT).show()
+            val locationData = JSONObject()
+            locationData.put("beat_officer", officerID)
+            locationData.put("latitude", latitude)
+            locationData.put("longitude", longitude)
+            locationData.put("accuracy", accuracy)
+            locationData.put("speed", speed)
+            locationData.put("heading", heading)
+            locationData.put("altitude", altitude)
+            locationData.put("utc_timestamp", Helper.getUTC())
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val clientNT = OkHttpClient().newBuilder().build()
+                    val request = API.post(URL.LOCATION_UPDATE, locationData, token)
+                    val response = clientNT.newCall(request).execute()
+                    if (response.isSuccessful) {
+                        Log.d("Railmaithri", altitude)
+                    }
+                } catch (_: Exception) {
+                }
+            }
+
+        } catch (_: Exception) {
+        }
+    }
+
+    private val locationStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            saveLocationDataToServer(
+                intent.getDoubleExtra("latitude",0.0).toString()!!,
+                intent.getDoubleExtra("longitude",0.0).toString()!!,
+                intent.getDoubleExtra("accuracy",0.0).toString()!!,
+                intent.getDoubleExtra("speed",0.0).toString()!!,
+                intent.getDoubleExtra("heading",0.0).toString()!!,
+                intent.getDoubleExtra("altitude",0.0).toString()!!,
+            )
+        }
+    }
+
+    override fun onResume() {
+        val intentFilter = IntentFilter("Location")
+        registerReceiver(locationStateReceiver, intentFilter)
+        super.onResume()
     }
 }
---
+++ app/src/main/java/gov/keralapolice/railmaithri/services/TrackingService.java
@@ -0,0 +1,197 @@
+package gov.keralapolice.railmaithri.services;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import gov.keralapolice.railmaithri.Home;
+import gov.keralapolice.railmaithri.R;
+
+public class TrackingService extends Service {
+
+    public Location previousBestLocation = null;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+    public LocationManager locationManager;
+    public MyLocationListener listener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not Yet Implemented");
+    }
+
+    private void startLocationService() {
+        String channelId = "location_notification_channel";
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent(getApplicationContext(), Home.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                0,
+                resultIntent,
+                PendingIntent.FLAG_MUTABLE
+        );
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                getApplicationContext(),
+                channelId
+        );
+        builder.setSmallIcon(R.drawable.ic_open_map);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null
+                    && notificationManager.getNotificationChannel(channelId) == null) {
+                CharSequence name;
+                NotificationChannel notificationChannel = new NotificationChannel(
+                        channelId,
+                        "Location Service",
+                        NotificationManager.IMPORTANCE_HIGH
+                );
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        listener = new MyLocationListener();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 2, (LocationListener) listener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 2, listener);
+
+        startForeground(175, builder.build());
+    }
+
+    private void stopLocationService() {
+        locationManager.removeUpdates(listener);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null) {
+                if (action.equals("startLocationService")) {
+                    startLocationService();
+                } else if (action.equals("stopLocationService")) {
+                    stopLocationService();
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+    public class MyLocationListener implements LocationListener {
+
+        public void onLocationChanged(final Location loc) {
+            if (isBetterLocation(loc, previousBestLocation)) {
+                //Send Coordinates to server
+                System.out.println(loc.getLatitude() + "|" + loc.getLongitude());
+                Toast.makeText(TrackingService.this, loc.getLatitude() + "|" + loc.getLongitude() + loc.getAccuracy() + loc.getSpeed() + loc.getBearing() + loc.getAltitude(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("Location");
+                intent.putExtra("latitude", loc.getLatitude()).toString();
+                intent.putExtra("longitude", loc.getLongitude()).toString();
+                intent.putExtra("accuracy", loc.getAccuracy()).toString();
+                intent.putExtra("speed", loc.getSpeed()).toString();
+                intent.putExtra("heading", loc.getBearing()).toString();
+                intent.putExtra("altitude", loc.getAltitude()).toString();
+                sendBroadcast(intent);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT).show();
+        }
+
+
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
