193477c6d80ceca9745b2592ef3230ac196df140
Thinkal VB <thinkalvb@gmail.com>
2023-10-11 14:43:07 +0530

    Edit a beat entry note

--- app/src/main/java/gov/keralapolice/railmaithri/BeatDiary.kt
+++ app/src/main/java/gov/keralapolice/railmaithri/BeatDiary.kt
@@ -1,10 +1,8 @@
 package gov.keralapolice.railmaithri
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
@@ -13,7 +11,6 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import gov.keralapolice.railmaithri.Helper.Companion.loadFormData
 import org.json.JSONObject
-import org.w3c.dom.Text
 
 class BeatDiary : AppCompatActivity() {
     private lateinit var mode:          String
@@ -21,6 +18,8 @@ class BeatDiary : AppCompatActivity() {
 
     private lateinit var note:          EditText
     private lateinit var utcTime:       String
+    private lateinit var profile:       JSONObject
+    private lateinit var beatData:      JSONObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,36 +30,21 @@ class BeatDiary : AppCompatActivity() {
         actionBT     = findViewById(R.id.action)
         note         = findViewById(R.id.note)
         utcTime      = Helper.getUTC()
+        profile      = JSONObject(Helper.getData(this, Storage.PROFILE)!!)
+        beatData     = profile.getJSONObject("last_beat_assignment")
+        showAssignmentData()
 
-        val profile = JSONObject(Helper.getData(this, Storage.PROFILE)!!)
-        val beatData = profile.getJSONObject("last_beat_assignment")
-
-        val beatLabel = beatData.getString("beat_label")
-        val beatFrom = beatData.getString("from_time_label").slice(IntRange(0, 15)).replace("T", " ")
-        val beatTo = beatData.getString("to_time_label").slice(IntRange(0, 15)).replace("T", " ")
-        val assignmentNote = beatData.getString("assignment_note")
-        val assignmentOn = beatData.getString("assigned_on").slice(IntRange(0, 15)).replace("T", " ")
-
-        val assignmentNoteTV = findViewById<TextView>(R.id.assignmentNote)
-        val durationTV = findViewById<TextView>(R.id.duration)
-        val dutyNoteTV = findViewById<TextView>(R.id.dutyNote)
-        val assignedOnTV = findViewById<TextView>(R.id.addedOn)
-        assignmentNoteTV.text = assignmentNote
-        durationTV.text = "${beatFrom} <-> ${beatTo}"
-        dutyNoteTV.text = beatLabel
-        assignedOnTV.text = assignmentOn
-
-        val serverNotesLY = findViewById<LinearLayout>(R.id.serverNoteList)
-        val savedNotesLY = findViewById<LinearLayout>(R.id.savedNoteList)
-        val beatDiary = loadFormData(this, Storage.BEAT_DIARY)
-        val bdKeys    = beatDiary.keys()
+        val serverNotesLY  = findViewById<LinearLayout>(R.id.serverNoteList)
+        val savedNotesLY   = findViewById<LinearLayout>(R.id.savedNoteList)
+        val beatDiary       = loadFormData(this, Storage.BEAT_DIARY)
+        val bdKeys= beatDiary.keys()
         while (bdKeys.hasNext()) {
-            val bdKeys  = bdKeys.next()
-            val value   = beatDiary.getJSONObject(bdKeys)
-            val button  = BeatDiary.generateButton(this, value, Mode.UPDATE_FORM)
+            val bdKeys   = bdKeys.next()
+            val value = beatDiary.getJSONObject(bdKeys)
+            val button   = generateButton(this, value, Mode.UPDATE_FORM)
             savedNotesLY.addView(button)
         }
-        
+
         prepareActionButton()
         actionBT.setOnClickListener { performAction() }
 
@@ -71,13 +55,24 @@ class BeatDiary : AppCompatActivity() {
         }
     }
 
+    private fun showAssignmentData() {
+        val beatLabel = beatData.getString("beat_label")
+        val beatFrom = beatData.getString("from_time_label").slice(IntRange(0, 15)).replace("T", " ")
+        val beatTo = beatData.getString("to_time_label").slice(IntRange(0, 15)).replace("T", " ")
+        val assignmentNote = beatData.getString("assignment_note")
+        val assignmentOn = beatData.getString("assigned_on").slice(IntRange(0, 15)).replace("T", " ")
+
+        findViewById<TextView>(R.id.assignmentNote).text = assignmentNote
+        findViewById<TextView>(R.id.duration).text = "${beatFrom} <-> ${beatTo}"
+        findViewById<TextView>(R.id.dutyNote).text = beatLabel
+        findViewById<TextView>(R.id.addedOn).text = assignmentOn
+    }
+
     private fun performAction() {
         if (mode == Mode.NEW_FORM || mode == Mode.UPDATE_FORM) {
             val formData = getFormData()
             if (formData != null) {
-                val profile = JSONObject(Helper.getData(this, Storage.PROFILE)!!)
-                val beatID  = profile.getJSONObject("last_beat_assignment").getInt("id")
-
+                val beatID  = beatData.getInt("id")
                 formData.put("beat_assignment", beatID)
                 formData.put("utc_timestamp", utcTime)
                 Helper.saveFormData(this, formData, Storage.BEAT_DIARY, utcTime)
@@ -106,24 +101,21 @@ class BeatDiary : AppCompatActivity() {
         }
     }
 
-    companion object{
-        fun generateButton(context: Context, formData: JSONObject, mode: String? = Mode.VIEW_FORM): Button {
-            val noteID           = formData.optString("id", "Diary Notes")
-            val createdOn        = formData.getString("utc_timestamp")
-                .take(16).replace("T", "\t")
-            val shortData = "ID ${noteID}\nDate: $createdOn"
+    fun generateButton(context: Context, formData: JSONObject, mode: String? = Mode.VIEW_FORM): Button {
+        val createdOn  = formData.getString("utc_timestamp").take(16).replace("T", "\t")
+        val description= formData.getString("description")
 
-            val button = Button(context)
-            button.isAllCaps = false
-            button.gravity = Gravity.START
-            button.text = shortData
-            button.setOnClickListener {
-                val intent = Intent(context,  BeatDiary::class.java)
-                intent.putExtra("mode", mode)
-                intent.putExtra("data", formData.toString())
-                context.startActivity(intent)
-            }
-            return button
+        val shortData = "$createdOn\n$description"
+        val button    = Button(context)
+        button.isAllCaps = false
+        button.gravity   = Gravity.START
+        button.text      = shortData
+
+        button.setOnClickListener {
+            utcTime   = formData.getString("utc_timestamp")
+            note.setText(formData.getString("description"))
         }
+        return button
     }
+
 }
